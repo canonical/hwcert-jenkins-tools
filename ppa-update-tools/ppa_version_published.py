7cf43d6f2c49931a48a874dd87e0ce45aac31663
Fernando Bravo <39527354+fernando79513@users.noreply.github.com>
2024-01-30 10:20:11 +0100

    Add wait for debs (#10)
    * Added wait for debs mechanism
    
    * Added tests to ppa_version_published
    
    * Updated README
    
    * Apply suggestions from code review
    
    Co-authored-by: kissiel <maciej.kisielewski@canonical.com>
    
    * Resolved PR comments
    
    * Fix version naming
    
    * Added exclusion for 18.04 + riscv64 to yaml file
    
    * Renamed fields in package source
    
    * Replaced exclusion of versions for inclusion
    
    Tests also modified to fit the new structure
    
    * Removed include special handling
    
    ---------
    
    Co-authored-by: kissiel <maciej.kisielewski@canonical.com>

---
+++ ppa-update-tools/ppa_version_published.py
@@ -0,0 +1,158 @@
+#!/usr/bin/env python3
+"""
+This program checks whether a specified package is available in a given PPA.
+for a the PPA correspondent to a snap channel
+
+The matrix of all combinations is created and the program periodically checks
+whether all combinations are available and waits until either all of them are
+available or the timeout is reached.
+The characteristics that can be specified are:
+  - (required and one) name of the source
+  - (required and one) name of the package (.deb file)
+  - (required and 1 or more) versions of ubuntu
+  - (required and 1 or more) architectures
+  - (optional and 1 or more) excluded combinations of ubuntu version and arch
+"""
+
+
+import argparse
+import requests
+import sys
+import time
+import yaml
+from typing import NamedTuple
+
+
+# the nameduple for the ppa specification
+class PackageSpec(NamedTuple):
+    source: str
+    package: str
+    version: str
+    ubuntu_version: str
+    arch: str
+
+
+def url_header_check(url: str) -> bool:
+    """
+    Check whether the url header is available.
+    :param url: the url to check
+    :return: True if the header is available, False otherwise
+    """
+    try:
+        response = requests.head(url)
+        if response.status_code == 200:
+            return True
+    except requests.ConnectionError:
+        print(f"Failed to connect to the URL: {url}")
+    return False
+
+
+def get_package_specs(yaml_content: dict, version: str) -> list[PackageSpec]:
+    """
+    Create a list of PackageSpec objects from the yaml content.
+    :param yaml_content: file containing the package requirements
+    :param version: the version of the package
+    :return: the list of PackageSpec objects
+    """
+
+    package_specs = []
+    for package in yaml_content["required-packages"]:
+        # General versions and architectures
+        for ubuntu_version in package["versions"]:
+            for arch in package["architectures"]:
+                package_specs.append(
+                    PackageSpec(
+                        package["source"],
+                        package["package"],
+                        version,
+                        ubuntu_version,
+                        arch,
+                    )
+                )
+    package_specs.sort()
+    return package_specs
+
+
+def check_packages_availability(
+    package_specs: list[PackageSpec], channel: str, timeout: int
+) -> None:
+    """
+    Iterate over the list of packages and check whether they are available
+    in the ppa.
+    :param package_specs: the list of packages to check
+    :param channel: the ppa correspondent to the channel
+    :param timeout: the timeout in seconds
+    """
+    # Dict to store whether each ppa is available.
+    already_available = {package_spec: False for package_spec in package_specs}
+    # Set the deadline.
+    deadline = time.time() + timeout
+
+    base_url = (
+        f"http://ppa.launchpad.net/checkbox-dev/{channel}/ubuntu/pool/main/c/"
+    )
+    while True:
+        for package_spec in package_specs:
+            if already_available[package_spec]:
+                continue
+            url = (
+                f"{base_url}{package_spec.source}/{package_spec.package}"
+                f"_{package_spec.version}~ubuntu{package_spec.ubuntu_version}"
+                f".1_{package_spec.arch}.deb"
+            )
+            already_available[package_spec] = url_header_check(url)
+
+        # Exit the loop if all packages are found.
+        if all(already_available.values()):
+            break
+
+        not_available = [
+            package_spec
+            for package_spec, is_available in already_available.items()
+            if not is_available
+        ]
+        print("Not all packages were available in the store.")
+        print("Here is the list of packages that were not found:")
+
+        for package_spec in not_available:
+            print(
+                f"{package_spec.package:<40} | "
+                f"ubuntu version {package_spec.ubuntu_version} | "
+                f"architecture {package_spec.arch}"
+            )
+        if time.time() > deadline:
+            raise SystemExit("Timeout reached.")
+
+        print("Waiting 30 seconds before retrying.")
+        # Wait before the next iteration.
+        time.sleep(30)
+    print("All packages were found.")
+
+
+def main(argv):
+    parser = argparse.ArgumentParser(
+        description="Check whether packages are available in the ppa"
+    )
+    parser.add_argument("version", help="Version of the packages to check for")
+    parser.add_argument(
+        "yaml_file",
+        type=argparse.FileType("r"),
+        help="Path to the YAML file specifying the package requirements",
+    )
+    parser.add_argument(
+        "--timeout",
+        help="Timeout in seconds after which the program will stop checking",
+        default=300,
+        type=float,
+    )
+    args = parser.parse_args(argv[1:])
+
+    yaml_content = yaml.load(args.yaml_file, Loader=yaml.FullLoader)
+    package_specs = get_package_specs(yaml_content, args.version)
+
+    channel = yaml_content["channel"]
+    check_packages_availability(package_specs, channel, args.timeout)
+
+
+if __name__ == "__main__":
+    main(sys.argv)
---
+++ ppa-update-tools/test_ppa_version_published.py
@@ -0,0 +1,177 @@
+import requests
+import unittest
+import yaml
+
+from unittest.mock import patch, mock_open
+
+
+from ppa_version_published import (
+    PackageSpec,
+    url_header_check,
+    get_package_specs,
+    main,
+)
+
+
+class TestIsPackageAvailable(unittest.TestCase):
+    url_example = (
+        "http://ppa.launchpad.net/checkbox-dev/edge/ubuntu/pool/main/c/"
+        "test/test-deb_1.0_amd64.deb"
+    )
+
+    @patch("requests.head")
+    def test_successful_request(self, mock_head):
+        # Mocking the response from launchpad
+        mock_response = requests.Response()
+        mock_response.status_code = 200
+        mock_head.return_value = mock_response
+        result = url_header_check(self.url_example)
+
+        self.assertTrue(result)
+
+    @patch("requests.head")
+    def test_no_package_request(self, mock_head):
+        # Mocking the response from launchpad
+        mock_response = requests.Response()
+        mock_response.status_code = 404
+        mock_head.return_value = mock_response
+        result = url_header_check(self.url_example)
+
+        self.assertFalse(result)
+
+    @patch("requests.head")
+    def test_request_raises_error(self, mock_head):
+        # Mocking the response from launchpad
+        mock_head.side_effect = requests.ConnectionError
+        result = url_header_check(self.url_example)
+        self.assertFalse(result)
+
+
+class TestGetPackageSpecs(unittest.TestCase):
+    def setUp(self):
+        self.sample_yaml_content = {
+            "channel": "edge",
+            "required-packages": [
+                {
+                    "source": "src1",
+                    "package": "pkg1",
+                    "versions": ["20.04", "22.04"],
+                    "architectures": ["amd64", "armhf"],
+                },
+                {
+                    "source": "src1",
+                    "package": "pkg1",
+                    "versions": ["18.04"],
+                    "architectures": ["amd64"],
+                },
+                {
+                    "source": "src2",
+                    "package": "pkg2",
+                    "versions": ["20.04", "22.04"],
+                    "architectures": ["all"],
+                },
+            ],
+        }
+
+    def test_get_package_specs(self):
+        package_specs = get_package_specs(self.sample_yaml_content, "1.0~dev1")
+
+        # 7 PackageSpec objects are created:
+        #   - 4 packages plus 1 included for src1
+        #   - 2 package for src2
+        self.assertEqual(len(package_specs), 7)
+
+        # The packages are sorted
+        self.assertEqual(package_specs[0].source, "src1")
+        self.assertEqual(package_specs[0].package, "pkg1")
+        self.assertEqual(package_specs[0].version, "1.0~dev1")
+        self.assertEqual(package_specs[0].ubuntu_version, "18.04")
+        self.assertEqual(package_specs[0].arch, "amd64")
+
+        # The inclusion adds only the intended package
+        self.assertFalse(
+            PackageSpec("src1", "pkg1", "1.0~dev1", "18.04", "armhf")
+            in package_specs
+        )
+
+
+class TestMainFunction(unittest.TestCase):
+    @patch("ppa_version_published.yaml.load")
+    @patch("ppa_version_published.check_packages_availability")
+    def test_argument_parsing(self, mock_check_packages, mock_yaml_load):
+        argv = ["script_name", "1.0", "path/to/file.yaml", "--timeout", "100"]
+
+        mock_yaml_load.return_value = {
+            "channel": "edge",
+            "required-packages": [
+                {
+                    "source": "src1",
+                    "package": "pkg1",
+                    "versions": ["1.0", "2.0"],
+                    "architectures": ["amd64", "armhf"],
+                },
+                {
+                    "source": "src1",
+                    "package": "pkg1",
+                    "versions": ["18.04"],
+                    "architectures": ["amd64"],
+                },
+                {
+                    "source": "src2",
+                    "package": "pkg2",
+                    "versions": ["1.0", "2.0"],
+                    "architectures": ["all"],
+                },
+            ],
+        }
+
+        m = mock_open()
+        with patch("builtins.open", m):
+            main(argv)
+
+        # check_packages_availability is called with the expected arguments
+        # 7 PackageSpec objects are created:
+        #   - 4 packages plus 1 included for src1
+        #   - 2 package for src2
+        self.assertEqual(len(mock_check_packages.call_args[0][0]), 7)
+        # The channel is edge
+        self.assertEqual(
+            mock_check_packages.call_args[0][1], "edge"
+        )  # timeout value
+        # The timeout is 100
+        self.assertEqual(
+            mock_check_packages.call_args[0][2], 100
+        )  # timeout value
+
+    @patch("ppa_version_published.yaml.load")
+    @patch("ppa_version_published.check_packages_availability")
+    def test_default_timeout(self, mock_check_packages, mock_yaml_load):
+        # Sample arguments without specifying timeout
+        argv = ["script_name", "1.0", "path/to/file.yaml"]
+
+        sample_yaml_content = """
+        channel: edge
+        required-packages:
+          - source: src1
+            package: pkg1
+            versions: ["1.0", "2.0"]
+            architectures: ["amd64", "armhf"]
+          - source: src1
+            package: pkg1
+            versions: ["3.0"]
+            architectures: ["amd64""]
+        """
+
+        m = mock_open(read_data=sample_yaml_content)
+
+        with patch("builtins.open", m):
+            # Mocking yaml.load to just return a dictionary based on the
+            # sample content
+            mock_yaml_load.return_value = yaml.safe_load(sample_yaml_content)
+            main(argv)
+
+        # Ensure check_packages_availability is called with default timeout
+        # value of 300
+        self.assertEqual(
+            mock_check_packages.call_args[0][2], 300
+        )  # default timeout value
