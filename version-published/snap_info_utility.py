2a9180c971060faceba3191c0c730df14bcaa089
ferbraher <fernando.bravo.hernandez@canonical.com>
2024-07-31 16:57:29 +0200

    fixed snap info with unit tests

--- version-published/snap_info_utility.py
+++ version-published/snap_info_utility.py
@@ -2,7 +2,11 @@
 This module contains every utility function shared among multiple
 scripts that fetches information about snaps
 """
+
+import re
 import requests
+
+from packaging import version
 from subprocess import check_output
 
 
@@ -38,16 +42,57 @@ def get_history_since(tag: str, repo_path: str):
     ).splitlines()
 
 
-def get_offset_from_version(version: str) -> int:
-    if "dev" not in version:
-        return 0
-    return int(version.rsplit("dev", 1)[1])
+def get_version_and_offset(version_str: str):
+    # Use regex to match the version pattern and extract the base version
+    # and dev number if present (e.g. v1.2.3-dev45, 1.2.3.dev45, 1.2.3)
+    # the v at the beginning is optional
+    match = re.match(r"^v?(\d+\.\d+\.\d+).?(?:dev(\d+))?$", version_str)
+    if match:
+        base_version = f"v{match.group(1)}"
+        dev_number = match.group(2) if match.group(2) else "0"
+        return base_version, int(dev_number)
+    else:
+        raise ValueError(f"Invalid version format: {version_str}")
 
 
-def get_revision_at_offset(version: str, repo_path: str):
-    tag = get_latest_tag(repo_path)
-    history = get_history_since(tag, repo_path)
-    offset = get_offset_from_version(version)
+def get_previous_tag(base_version: str, repo_path: str):
+    # Get the list of tags sorted by creation date
+    tags = check_output(
+        ["git", "tag", "--sort=-creatordate"], cwd=repo_path, text=True
+    ).splitlines()
+
+    # Filter the list of tags to only include the ones that match the version
+    # pattern
+    tags = [tag for tag in tags if re.match(r"^v\d+\.\d+\.\d+$", tag)]
+    print(tags)
+
+    # Get the previous tag corresponding to the base version. We have to do it
+    # this way because the tags are only created once the version is published.
+    # For example, 4.0.0.dev333 will use the previous tag v3.3.0 to calculate
+    # the offset, not v4.0.0. The versions after 4.0.0 will use v4.0.0.
+    previous_tag = None
+    for t in tags:
+        print(f"Comparing {t} with {base_version}")
+        if version.parse(t) < version.parse(base_version):
+            print(f"Found previous tag: {t}")
+            previous_tag = t
+            break
+
+    if not previous_tag:
+        raise SystemExit(
+            f"Unable to locate a previous tag for the version: {base_version}"
+        )
+
+    return previous_tag
+
+
+def get_revision_at_offset(version_str: str, repo_path: str):
+    base_version, offset = get_version_and_offset(version_str)
+    previous_tag = get_previous_tag(base_version, repo_path)
+    history = get_history_since(previous_tag, repo_path)
+    print(
+        f"Checkout to {offset} commits after the preceding tag {previous_tag}"
+    )
     # history is HEAD -> latest_tag(included)
     # reverse it so it tag -> HEAD
     history = list(reversed(history))
@@ -58,11 +103,5 @@ def get_revision_at_offset(version: str, repo_path: str):
         return history[offset]
     except IndexError:
         raise SystemExit(
-            "Unable to locate the commit that generated version: ({version})"
+            f"Unable to locate the commit that generated version: {version_str}"
         )
-
-
-def get_latest_tag(repo_path: str):
-    return check_output(
-        ["git", "describe", "--tags", "--abbrev=0"], cwd=repo_path, text=True
-    ).strip()
--- version-published/test_snap_info_utility.py
+++ version-published/test_snap_info_utility.py
@@ -1,28 +1,65 @@
 import unittest
 from unittest.mock import patch, call
 
+import textwrap
+
 import snap_info_utility
 
 
 class TestSnapInfoUtility(unittest.TestCase):
-    def test_get_offset_from_version(self):
+    def test_get_version_and_offset(self):
         version = "v1.2.3-dev45"
+        b_version, offset = snap_info_utility.get_version_and_offset(version)
+        self.assertEqual(b_version, "v1.2.3")
+        self.assertEqual(offset, 45)
 
-        result = snap_info_utility.get_offset_from_version(version)
+        version = "1.2.3"
+        b_version, offset = snap_info_utility.get_version_and_offset(version)
+        self.assertEqual(b_version, "v1.2.3")
+        self.assertEqual(offset, 0)
 
-        self.assertEqual(result, 45)
+        version = "1.2.3.dev45"
+        b_version, offset = snap_info_utility.get_version_and_offset(version)
+        self.assertEqual(b_version, "v1.2.3")
+        self.assertEqual(offset, 45)
 
-    @patch("snap_info_utility.get_latest_tag")
+        version = "v1.2"
+        with self.assertRaises(ValueError):
+            snap_info_utility.get_version_and_offset(version)
+
+        version = "1.2.3-dv25"
+        with self.assertRaises(ValueError):
+            snap_info_utility.get_version_and_offset(version)
+
+    @patch("snap_info_utility.check_output")
+    def test_get_previous_tag(self, mock_check_output):
+        mock_check_output.return_value = textwrap.dedent(
+            """
+            v1.2.3
+            v1.2.2
+            v1.2.1
+            """
+        )
+        result = snap_info_utility.get_previous_tag("v1.2.3", "/path/to/repo")
+        self.assertEqual(result, "v1.2.2")
+
+        result = snap_info_utility.get_previous_tag("v1.2.2", "/path/to/repo")
+        self.assertEqual(result, "v1.2.1")
+
+        with self.assertRaises(SystemExit):
+            snap_info_utility.get_previous_tag("v1.0.0", "/path/to/repo")
+
+    @patch("snap_info_utility.get_previous_tag")
     @patch("snap_info_utility.get_history_since")
     def test_get_revision_at_offset(
-        self, mock_get_history_since, mock_get_latest_tag
+        self, mock_get_history_since, mock_get_previous_tag
     ):
-        mock_get_latest_tag.return_value = "v1.0.0"
+        mock_get_previous_tag.return_value = "v1.0.0"
         mock_get_history_since.return_value = [
             "tag_hash + 3",
             "tag_hash + 2",
             "tag_hash + 1",
-            "tag_hash"
+            "tag_hash",
         ]
 
         result = snap_info_utility.get_revision_at_offset(
@@ -30,3 +67,21 @@ class TestSnapInfoUtility(unittest.TestCase):
         )
 
         self.assertEqual(result, "tag_hash + 2")
+
+    @patch("snap_info_utility.get_previous_tag")
+    @patch("snap_info_utility.get_history_since")
+    def test_get_revision_at_offset_error(
+        self, mock_get_history_since, mock_get_previous_tag
+    ):
+        mock_get_previous_tag.return_value = "v1.0.0"
+        mock_get_history_since.return_value = [
+            "tag_hash + 3",
+            "tag_hash + 2",
+            "tag_hash + 1",
+            "tag_hash",
+        ]
+
+        with self.assertRaises(SystemExit):
+            snap_info_utility.get_revision_at_offset(
+                "v1.2.3-dev5", "/path/to/repo"
+            )
