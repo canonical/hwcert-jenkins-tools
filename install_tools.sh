00b9352670448612d8a171d5473ba2e9999f9122
George Boukeas <boukeas@users.noreply.github.com>
2024-09-23 09:33:50 +0100

    [CERTTF-358] Amend and extend scriptlets for snap-testing jobs (#42)
    * refactor: introduce `install_on_device` function in installer
    * fix: don't move ahead with installation on device if it is unresponsive
    * feat: disable tracing within the installer
    * feat: introduce `_get` scriptlet
    * feat: display a message when `check_for_snap_changes` completes successfully
    * feat: restrict the output of `check_for_snap_changes` to ongoing changes
    * feat: make `_put` and `_get` more forgiving when : is not used
    * chore: in the installer, perform `wait_for_ssh` before displaying message
    * feat: `wait_for_snap_changes` also waits for pending tasks
    * fix: be more permissive when the installer first runs `wait_for_ssh`

--- install_tools.sh
+++ install_tools.sh
@@ -4,44 +4,16 @@
 # If the repo is already available locally, fetch the latest version.
 # Use the --branch option to specify a specific branch
 
+# disable tracing (if previously enabled)
+[[ "$-" == *x* ]] && TRACING=true && set +x || TRACING=false
+
 TOOLS_REPO=https://github.com/canonical/hwcert-jenkins-tools.git
 TOOLS_PATH_DEFAULT=$(basename $TOOLS_REPO .git)
+BRANCH_DEFAULT=main
 
 usage() {
     echo "Usage: $0 [<path>] [--branch <value>]"
     exit 1
-}
-
-parse_args() {
-    TOOLS_PATH=""
-    BRANCH=""
-    while [[ "$#" -gt 0 ]]; do
-        case $1 in
-            --branch)
-                if [ -n "$2" ]; then
-                    BRANCH=$2
-                    shift
-                else
-                    echo "Error: value required for --branch"
-                    exit 1
-                fi
-                ;;
-            *)
-                if [ -z "$TOOLS_PATH" ]; then
-                    TOOLS_PATH=$1
-                else
-                    echo "Error: Invalid argument $1"
-                    usage
-                fi
-                ;;
-        esac
-        shift
-    done
-    # assign default values if command-line arguments have not been provided
-    TOOLS_PATH=${TOOLS_PATH:-$TOOLS_PATH_DEFAULT}
-    BRANCH=${BRANCH:-main}
-    export TOOLS_PATH
-    export BRANCH
 }
 
 fetch() {
@@ -55,7 +27,53 @@ clone() {
     echo "Cloned $TOOLS_REPO@$BRANCH into local repo: $TOOLS_PATH"
 }
 
-parse_args $@
+install_on_device() {
+    # figure out where to place scriptlets on the device
+    REMOTE_PATH=$(cat $SCRIPTLETS_PATH/defs/scriptlet_path | _run bash)
+    RESULT=$?
+    [ "$RESULT" -eq 0 ] || return $RESULT
+
+    # copy selected scriptlets over to the device
+    # and then move them somewhere in the device's PATH
+    DEVICE_SCRIPTLETS=(retry check_for_packages_complete wait_for_packages_complete install_packages)
+    _put "${DEVICE_SCRIPTLETS[@]/#/$SCRIPTLETS_PATH/}" :
+    _run sudo mv "${DEVICE_SCRIPTLETS[@]}" $REMOTE_PATH
+
+    # fuser is required by `check_for_packages_complete`
+    # (so install it on the device, where it is used)
+    _run bash < $SCRIPTLETS_PATH/defs/install_psmisc
+}
+
+TOOLS_PATH=""
+BRANCH=""
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --branch)
+            if [ -n "$2" ]; then
+                BRANCH=$2
+                shift
+            else
+                echo "Error: value required for --branch"
+                exit 1
+            fi
+            ;;
+        *)
+            if [ -z "$TOOLS_PATH" ]; then
+                TOOLS_PATH=$1
+            else
+                echo "Error: Invalid argument $1"
+                usage
+            fi
+            ;;
+    esac
+    shift
+done
+
+# assign default values if command-line arguments have not been provided
+TOOLS_PATH=${TOOLS_PATH:-$TOOLS_PATH_DEFAULT}
+BRANCH=${BRANCH:-$BRANCH_DEFAULT}
+
+# retrieve the tools from the repository
 fetch || (rm -rf $TOOLS_PATH && clone)
 
 # add scriptlets to agent's PATH
@@ -63,25 +81,17 @@ SCRIPTLETS_PATH=$TOOLS_PATH/scriptlets
 source "$SCRIPTLETS_PATH/defs/add_to_path"
 add_to_path $SCRIPTLETS_PATH
 add_to_path $SCRIPTLETS_PATH/sru-helpers
-
-# figure out where to place scriptlets on the device
-REMOTE_PATH=$(cat $SCRIPTLETS_PATH/defs/scriptlet_path | _run bash)
-[ $? -eq 0 ] || exit 1
-
-# copy selected scriptlets over to the device
-# and then move them somewhere in the device's PATH
-DEVICE_SCRIPTLETS=(retry check_for_packages_complete wait_for_packages_complete install_packages)
-_put "${DEVICE_SCRIPTLETS[@]/#/$SCRIPTLETS_PATH/}" :
-_run sudo mv "${DEVICE_SCRIPTLETS[@]}" $REMOTE_PATH
-
-# fuser is required by `check_for_packages_complete`
-# (so install it on the device, where it is used)
-_run bash < $SCRIPTLETS_PATH/defs/install_psmisc
-[ $? -eq 0 ] || exit 1
-
-#
 add_to_path ~/.local/bin
-install_packages pipx python3-venv
+
+wait_for_ssh --allow-degraded \
+&& echo "Installing selected scriptlets on the device" \
+&& install_on_device \
+|| exit 1
 
 # install launcher tool on the agent
-pipx install --spec $TOOLS_PATH/cert-tools/launcher launcher
+echo "Installing pipx"
+install_packages pipx python3-venv > /dev/null
+pipx install --spec $TOOLS_PATH/cert-tools/launcher launcher > /dev/null
+
+# restore tracing (if previously enabled)
+[ "$TRACING" = true ] && set -x || true
