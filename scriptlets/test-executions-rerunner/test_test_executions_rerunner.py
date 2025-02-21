b840309c82f6d85a75609456fab20b5d04f645e8
George Boukeas <george.boukeas@canonical.com>
2025-02-21 20:00:54 +0000

    chore: consolidate tests

--- scriptlets/test-executions-rerunner/test_test_executions_rerunner.py
+++ scriptlets/test-executions-rerunner/test_test_executions_rerunner.py
@@ -3,182 +3,247 @@ import pytest
 import requests_mock
 
 from test_executions_rerunner import (
-    Jenkins, Github, Rerunner, TestObserverInterface
+    Jenkins, Github,
+    Rerunner, RequestProccesingError,
+    TestObserverInterface
 )
 
-jenkins_request_headers = {
-    "Authorization": f"Basic {base64.b64encode(b'admin:jtoken').decode()}"
+
+# collection of tests to check that the Jenkins and Github
+# request processors fail when they should
+
+def test_jenkins_no_ci_link():
+    rerun_request = {
+        "test_execution_id": 1,
+        "family": "deb"
+    }
+    with pytest.raises(RequestProccesingError):
+        Jenkins.process(rerun_request)
+
+
+@pytest.mark.parametrize(
+    "ci_link",
+    [
+        "http://10.102.156.15:8080/job/fake-job/"
+        "http://10.102.156.15:8080/job/fake-job/1/2"
+        "https://github.com/canonical/fake-repo/actions/runs/13/job/39",
+        "invalid-url",
+    ]
+)
+def test_jenkins_invalid_ci_link(ci_link):
+    rerun_request = {
+        "test_execution_id": 1,
+        "ci_link": ci_link,
+        "family": "deb"
+    }
+    with pytest.raises(RequestProccesingError):
+        Jenkins.process(rerun_request)
+
+
+def test_jenkins_no_family():
+    job_link = "http://10.102.156.15:8080/job/fake-job"
+    rerun_request = {
+        "test_execution_id": 1,
+        "ci_link": f"{job_link}/123",
+    }
+    with pytest.raises(RequestProccesingError):
+        Jenkins.process(rerun_request)
+
+
+def test_jenkins_invalid_family():
+    job_link = "http://10.102.156.15:8080/job/fake-job"
+    rerun_request = {
+        "test_execution_id": 1,
+        "ci_link": f"{job_link}/123",
+        "family": "image",
+    }
+    with pytest.raises(RequestProccesingError):
+        Jenkins.process(rerun_request)
+
+
+def test_github_no_ci_link():
+    rerun_request = {
+        "test_execution_id": 1,
+    }
+    with pytest.raises(RequestProccesingError):
+        Github.process(rerun_request)
+
+
+@pytest.mark.parametrize(
+    "ci_link",
+    [
+        "https://github.com/canonical/fake-repo/actions/runs/13",
+        "https://github.com/fake-owner/fake-repo/actions/runs/13/job/39",
+        "http://10.102.156.15:8080/job/fake-job/1",
+        "invalid-url",
+    ]
+)
+def test_github_invalid_ci_link(ci_link):
+    rerun_request = {
+        "test_execution_id": 1,
+        "ci_link": ci_link
+    }
+    with pytest.raises(RequestProccesingError):
+        Github.process(rerun_request)
+
+
+# miscellaneous pieces of data to help with tests;
+# these are not fixtures as they don't need to be recreated across tests 
+
+# what the headers towards Jenkins and Github should look like
+headers = {
+    "Jenkins": {
+        "Authorization": f"Basic {base64.b64encode(b'admin:jtoken').decode()}"
+    },
+    "Github": {
+        "Accept": "application/vnd.github+json",
+        "Authorization": "Bearer ghtoken"
+    },
 }
 
-reruns_link = TestObserverInterface().reruns_endpoint
+# a set of rerun requests, to emulate what might retrieved from Test Observer
+rerun_requests = [
+    {
+        "test_execution_id": 1,
+        "ci_link": "http://10.102.156.15:8080/job/snap-job/139",
+        "family": "snap"
+    },
+    {
+        "test_execution_id": 2,
+        "ci_link": "http://10.102.156.15:8080/job/deb-job/333",
+        "family": "deb"
+    },
+    {
+        "test_execution_id": 3,
+        "ci_link": "invalid-url",
+    },
+    {
+        "test_execution_id": 4,
+        "ci_link": "https://github.com/canonical/fake-repo/actions/runs/13/job/39",
+    },
+    {
+        "test_execution_id": 5,
+        "ci_link": "https://github.com/canonical/fake-repo/actions/runs/39/job/117",
+    },
+]
 
-@pytest.fixture(scope="session")
-def rerun_requests():
-    return [
-        {
-            "test_execution_id": 1,
-            "ci_link": "http://10.102.156.15:8080/job/snap-job/139",
-            "family": "snap"
-        },
-        {
-            "test_execution_id": 2,
-            "ci_link": "http://10.102.156.15:8080/job/deb-job/333",
-            "family": "deb"
-        },
-        {
-            "test_execution_id": 3,
-            "ci_link": "invalid-url",
-        },
-        {
-            "test_execution_id": 4,
-            "ci_link": "https://github.com/canonical/fake-repo/actions/runs/13/job/39",
-        },
-        {
-            "test_execution_id": 5,
-            "ci_link": "https://github.com/canonical/fake-repo/actions/runs/39/job/117",
-        },
-    ]
+# processors for rerun requests, i.e. interfaces towards Jenkins and Github
+jenkins = Jenkins("admin", "jtoken")
+github = Github("ghtoken")
+
+# the expected result of Rerunner.process_rerun_requests;
+# this allows one-to-one checking of how each rerun request is processed
+expected_processed = {
+    1: (
+            jenkins,
+            {
+                "url": "http://10.102.156.15:8080/job/snap-job/buildWithParameters",
+                "json": {"TEST_OBSERVER_REPORTING": True}
+            }
+    ),
+    2: (
+            jenkins,
+            {
+                "url": "http://10.102.156.15:8080/job/deb-job/buildWithParameters",
+                "json": {"TEST_OBSERVER_REPORTING": True, "TESTPLAN": "full"}
+            }
+    ),
+    4: (
+            github,
+            {
+                "url": "https://api.github.com/repos/canonical/fake-repo/actions/runs/13/rerun",
+            }
+    ),
+    5: (
+            github,
+            {
+                "url": "https://api.github.com/repos/canonical/fake-repo/actions/runs/39/rerun",
+            }
+    ),
+}
 
 
 @pytest.fixture
-def github():
-    return Github("ghtoken")
-
-@pytest.fixture
-def jenkins():
-    return Jenkins("admin", "jtoken")
-
-@pytest.fixture
-def rerunner(github, jenkins):
+def rerunner_mixed():
     return Rerunner([jenkins, github])
 
+@pytest.fixture
+def rerunner_jenkins_only():
+    return Rerunner([jenkins])
 
-def test_does_nothing_when_no_reruns_requested(rerunner, requests_mock):
-    requests_mock.get(reruns_link, json=[])
-
-    rerunner.run()
-
-    assert requests_mock.called_once
-
-
-def test_correctly_submits_deb_rerun_request(rerunner, requests_mock):
-    job_link = "http://10.102.156.15:8080/job/fake-job"
-
-    ci_link = f"{job_link}/1/"
-    requests_mock.get(
-        reruns_link,
-        json=[{"test_execution_id": 1, "ci_link": ci_link, "family": "deb"}],
-    )
-
-    def are_build_parameters_valid(request) -> bool:  # noqa: ANN001
-        return request.json() == {"TESTPLAN": "full", "TEST_OBSERVER_REPORTING": True}
-
-    rerun_link = f"{job_link}/buildWithParameters"
-    jenkins_build_matcher = requests_mock.post(
-        rerun_link,
-        request_headers=jenkins_request_headers,
-        additional_matcher=are_build_parameters_valid,
-    )
-
-    def is_delete_body_valid(request) -> bool:
-        return request.json() == {"test_execution_ids": [1]}
-
-    test_observer_delete_matcher = requests_mock.delete(
-        reruns_link,
-        additional_matcher=is_delete_body_valid,
-    )
-
-    rerunner.run()
-
-    assert jenkins_build_matcher.called_once
-    assert test_observer_delete_matcher.called_once
+@pytest.fixture
+def rerunner_github_only():
+    return Rerunner([github])
 
 
-def test_correctly_submits_snap_rerun_request(rerunner, requests_mock):
-    job_link = "http://10.102.156.15:8080/job/fake-job"
-
-    ci_link = f"{job_link}/1/"
-    requests_mock.get(
-        reruns_link,
-        json=[{"test_execution_id": 1, "ci_link": ci_link, "family": "snap"}],
-    )
-
-    def are_build_parameters_valid(request) -> bool:  # noqa: ANN001
-        return request.json() == {"TEST_OBSERVER_REPORTING": True}
-
-    rerun_link = f"{job_link}/buildWithParameters"
-    jenkins_build_matcher = requests_mock.post(
-        rerun_link,
-        request_headers=jenkins_request_headers,
-        additional_matcher=are_build_parameters_valid,
-    )
-
-    def is_delete_body_valid(request) -> bool:
-        return request.json() == {"test_execution_ids": [1]}
-
-    test_observer_delete_matcher = requests_mock.delete(
-        reruns_link,
-        additional_matcher=is_delete_body_valid,
-    )
-
-    rerunner.run()
-
-    assert jenkins_build_matcher.called_once
-    assert test_observer_delete_matcher.called_once
-
-
-################
-
-def test_rerunner_load(rerunner, rerun_requests):
+def test_does_nothing_when_no_reruns_requested(rerunner_mixed):
     with requests_mock.Mocker() as mocker:
-        matcher = mocker.get(reruns_link, status_code=200, json=rerun_requests)
-        loaded_rerun_requests = rerunner.load_rerun_requests()
-        assert matcher.called_once
-        assert loaded_rerun_requests == rerun_requests
+        catch_all = mocker.register_uri(requests_mock.ANY, requests_mock.ANY, status_code=500)
+        load_matcher = mocker.get(TestObserverInterface().reruns_endpoint, json=[])
+        rerunner_mixed.run()
+    assert not catch_all.called
+    assert load_matcher.called_once
 
 
-def test_rerunner_process(rerunner, rerun_requests):
-    processed = rerunner.process_rerun_requests(rerun_requests)
-    assert len(processed) == 4
-    assert 3 not in processed
+@pytest.mark.parametrize(
+    "rerunner_name, expected_matches, expected_successful", [
+        ("rerunner_jenkins_only", [1, 2], [1, 2]),
+        ("rerunner_github_only", [4, 5], [4]),
+        ("rerunner_mixed", [1, 2, 4, 5], [2, 5]),
+    ],
+)
+def test_end_to_end(request, rerunner_name, expected_matches, expected_successful):
+    rerunner = request.getfixturevalue(rerunner_name)
 
+    def create_json_matcher(post_arguments):
+        if "json" in post_arguments:
+            local_json = post_arguments["json"]
+            def json_matcher(request):
+                return request.json() == local_json
+        else:
+            def json_matcher(_):
+                return True
+        return json_matcher
 
-def test_rerunner_submit(rerunner, rerun_requests):
-    processed = rerunner.process_rerun_requests(rerun_requests)
+    def request_headers_filter(processor):
+        return headers[type(processor).__name__]
+
     with requests_mock.Mocker() as mocker:
-        mocker.post(requests_mock.ANY, status_code=500)
-        matchers = [
-            mocker.post(
-                "http://10.102.156.15:8080/job/snap-job/buildWithParameters",
-                additional_matcher=lambda request: request.json() == {"TEST_OBSERVER_REPORTING": True},
-                status_code=200,
-            ),
-            mocker.post(
-                "http://10.102.156.15:8080/job/deb-job/buildWithParameters",
-                additional_matcher=lambda request: request.json() == {"TEST_OBSERVER_REPORTING": True, "TESTPLAN": "full"},
-                status_code=200
-            ),
-            mocker.post(
-                "https://api.github.com/repos/canonical/fake-repo/actions/runs/13/rerun",
-                status_code=500
-            ),
-            mocker.post(
-                "https://api.github.com/repos/canonical/fake-repo/actions/runs/39/rerun",
-                status_code=200
-            ),
-        ]
-        successful = rerunner.submit_processed_requests(processed)
-    for matcher in matchers:
-        assert matcher.called_once
-    assert successful == [1, 2, 5]
-
-
-def test_rerunner_delete(rerunner):
-    successful = [1, 2, 5]
-    with requests_mock.Mocker() as mocker:
-        matcher = mocker.delete(
-            reruns_link,
-            additional_matcher=lambda request: request.json() == {"test_execution_ids": successful}
+        catch_all = mocker.register_uri(requests_mock.ANY, requests_mock.ANY, status_code=500)
+        # this will be used to mock-load the rerun requests
+        load_matcher = mocker.get(
+            TestObserverInterface().reruns_endpoint,
+            status_code=200,
+            json=rerun_requests
         )
-        rerunner.delete_rerun_requests(successful)
-        assert matcher.called_once
+        # mock the response to each one of the rerun triggers:
+        # the mocks have been specified to fully match the rerun requests,
+        # so if any of the POSTs are different than expected, they will
+        # match the `catch_all` instead and the test will fail
+        submit_matchers = [
+            mocker.post(
+                post_arguments["url"],
+                request_headers=request_headers_filter(processor),
+                additional_matcher=create_json_matcher(post_arguments),
+                # only some of the reruns are designated to succeed,
+                # so that we can check that only these are deleted
+                status_code=200 if execution_id in expected_successful else 500
+            )
+            for execution_id, (processor, post_arguments) in expected_processed.items()
+            if execution_id in expected_matches
+        ]
+        # this will be used to mock-delete the rerun requests that were
+        # processed and serviced successfully
+        delete_matcher = mocker.delete(
+            TestObserverInterface().reruns_endpoint,
+            additional_matcher=lambda request: request.json() == {"test_execution_ids": expected_successful}
+        )
+        rerunner.run()
+
+    assert not catch_all.called
+    assert load_matcher.called_once
+    assert all(
+        submit_matcher.called_once
+        for submit_matcher in submit_matchers
+    )
+    assert delete_matcher.called_once
